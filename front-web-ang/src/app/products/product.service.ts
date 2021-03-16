7c1ae22ded001b6538c37f9000b4c5f4585898b2
Alisson Youssf <youssfbr@gmail.com>
2021-03-15 21:24:12 -0300

    front prod

--- front-web-ang/src/app/products/product.service.ts
+++ front-web-ang/src/app/products/product.service.ts
@@ -1,16 +1,16 @@
 import { Injectable } from "@angular/core";
-
 import { HttpClient } from "@angular/common/http";
-import { Product } from "./product";
 import { Observable } from "rxjs";
+import { environment } from '../../environments/environment.prod';
 
+import { Product } from "./product";
 
 @Injectable()
 export class ProductService {
 
     constructor(private http: HttpClient) { }
 
-    protected UrlServiceV1: string = "http://localhost:3000/";
+    protected UrlServiceV1: string = environment.APP_URL;   
 
     getProducts() : Observable<Product[]> {
         return this.http
